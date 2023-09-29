c0ed0f314ad131b753f6a01f19aa2305915f2c4b
Jaro Hradsky <jaroslav.hradsky@gmail.com>
2023-09-29 20:41:47 +0200

    62. Adding the single post page

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'blog/index.html')
+
+def posts(request):
+    return render(request, 'blog/posts.html')
+
+def post_detail(request, slug):
+    return render(request, 'blog/post-detail.html')
