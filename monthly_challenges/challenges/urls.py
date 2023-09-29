09d0b3ba47a1aeb16fa54afa792b820a517eca50
Jaro Hradsky <jaroslav.hradsky@gmail.com>
2023-09-29 12:51:22 +0200

    includes/snippets -> navigation bar

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("<int:month>", views.monthly_challenge_by_number),  # if integers
     path("<str:month>", views.monthly_challenge, name='month-challenge'),  # if string
-    path("", views.index), 
+    path("", views.index, name='index'), 
 ]
