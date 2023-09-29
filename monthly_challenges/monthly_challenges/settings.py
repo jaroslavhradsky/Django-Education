d27cd97b6aefcb61375a08a587d2861e2de178f9
Jaro Hradsky <jaroslav.hradsky@gmail.com>
2023-09-29 12:32:10 +0200

    template inheritance

--- monthly_challenges/monthly_challenges/settings.py
+++ monthly_challenges/monthly_challenges/settings.py
@@ -55,7 +55,10 @@ ROOT_URLCONF = 'monthly_challenges.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        #'DIRS': [BASE_DIR / 'challenges' / 'templates'],
+        'DIRS': [
+            #[BASE_DIR / 'challenges' / 'templates'],
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
