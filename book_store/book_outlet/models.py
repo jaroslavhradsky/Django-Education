f1d3c18c37954ea3c0d3d1e07ead8446ce426653
Jaro Hradsky <jaroslav.hradsky@gmail.com>
2023-10-06 12:56:03 +0200

    book_store initial deploy

---
+++ book_store/book_outlet/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.CharField(null=True, max_length=20)
+    is_bestselling = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[self.id])
+
+    def __str__(self) -> str:
+        return f'{self.title} ({self.rating})'
+
+
---
+++ book_store/book_outlet/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+from django.http import Http404
+
+def index(request):
+    books = Book.objects.all()
+    return render(request, 'book_outlet/index.html', {
+        'books': books
+    })
+
+def book_detail(request, id):
+    try:
+        book = Book.objects.get(pk=id)
+    except:
+        raise Http404()
+    #book = get_object_or_404(Book, pk=id) # another alternative how to raise 404 if book does not exist
+    return render(request, 'book_outlet/book_detail.html', {
+        'title': book.title,
+        'author': book.author,
+        'rating': book.rating,
+        'is_bestseller': book.is_bestselling
+    })
