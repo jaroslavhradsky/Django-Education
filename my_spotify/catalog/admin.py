9e71704771be62c98b992b7c28ec2a17a7696c66
Jaro Hradsky <jaroslav.hradsky@gmail.com>
2023-10-13 14:27:25 +0200

    my_spotify add admin

--- my_spotify/catalog/admin.py
+++ my_spotify/catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Album
 
 # Register your models here.
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('interpret', 'name', 'image')
+    list_filter = ('interpret',)
+
+admin.site.register(Album, AlbumAdmin)
