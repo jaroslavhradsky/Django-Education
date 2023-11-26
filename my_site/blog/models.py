5c0e96ca82ecc127a84e1b1e0c1bd26f0323da76
Jaro Hradsky <jaroslav.hradsky@gmail.com>
2023-11-26 08:54:21 +0100

    blog - created models + superuser

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True) # Update when we save the object (last update)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
+    tags = models.ManyToManyField(Tag)
+
+
+
+
