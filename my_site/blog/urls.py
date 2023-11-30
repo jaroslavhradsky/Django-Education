06a197b97750e90dda8fe1bc6c1bd68903b5df28
Jaro Hradsky <jaroslav.hradsky@gmail.com>
2023-11-30 17:39:22 +0100

    blog - read later feature

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('posts', views.AllPostsView.as_view(), name='posts-page'),
     #path('posts/<slug:slug>', views.post_detail, name='post-detail-page'), # /posts/my-first-post
     path('posts/<slug:slug>', views.SinglePostView.as_view(), name='post-detail-page'), 
+    path('read-later', views.ReadLaterView.as_view(), name='read-later'), 
 ]
